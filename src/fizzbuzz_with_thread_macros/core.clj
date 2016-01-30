66c9d8df96d3d08389a709bc05b20b9750a570e7
trikitrok <trikitrok@gmail.com>
2016-01-30 15:17:09 +0100

    Added a bit of sugar

--- src/fizzbuzz_with_thread_macros/core.clj
+++ src/fizzbuzz_with_thread_macros/core.clj
@@ -3,16 +3,19 @@
 (defn- multiple-of? [div num]
   (zero? (mod num div)))
 
-(defn- transform-to [to & preds]
+(defn- transform-to [to preds]
   (fn [num]
     (if (every? #(% num) (cons (complement string?) preds))
       to
       num)))
 
+(defn- when-multiple-of [& divisors]
+  (map #(partial multiple-of? %) divisors))
+
 (defn fizzbuzz []
   (->>
     (range 1 101)
-    (map (transform-to "FizzBuzz" (partial multiple-of? 3) (partial multiple-of? 5)))
-    (map (transform-to "Fizz" (partial multiple-of? 3)))
-    (map (transform-to "Buzz" (partial multiple-of? 5)))
+    (map (transform-to "FizzBuzz" (when-multiple-of 3 5)))
+    (map (transform-to "Fizz" (when-multiple-of 3)))
+    (map (transform-to "Buzz" (when-multiple-of 5)))
     (map str)))
