434d5e1823b4ff0298bdedce08e64ba55c3c2aca
trikitrok <trikitrok@gmail.com>
2016-01-30 15:13:25 +0100

    Renamed function

--- src/fizzbuzz_with_thread_macros/core.clj
+++ src/fizzbuzz_with_thread_macros/core.clj
@@ -3,7 +3,7 @@
 (defn- multiple-of? [div num]
   (zero? (mod num div)))
 
-(defn- transform [to & preds]
+(defn- transform-to [to & preds]
   (fn [num]
     (if (every? #(% num) (cons (complement string?) preds))
       to
@@ -12,7 +12,7 @@
 (defn fizzbuzz []
   (->>
     (range 1 101)
-    (map (transform "FizzBuzz" (partial multiple-of? 3) (partial multiple-of? 5)))
-    (map (transform "Fizz" (partial multiple-of? 3)))
-    (map (transform "Buzz" (partial multiple-of? 5)))
+    (map (transform-to "FizzBuzz" (partial multiple-of? 3) (partial multiple-of? 5)))
+    (map (transform-to "Fizz" (partial multiple-of? 3)))
+    (map (transform-to "Buzz" (partial multiple-of? 5)))
     (map str)))
