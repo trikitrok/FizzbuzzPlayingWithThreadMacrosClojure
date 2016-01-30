971533ee8b3dbc9ad80a852312a3070680d51c37
trikitrok <trikitrok@gmail.com>
2016-01-30 15:03:50 +0100

    Extracted helper

--- src/fizzbuzz_with_thread_macros/core.clj
+++ src/fizzbuzz_with_thread_macros/core.clj
@@ -1,9 +1,12 @@
 (ns fizzbuzz-with-thread-macros.core)
 
+(defn- multiple-of? [num div]
+  (zero? (mod num div)))
+
 (defn fizzbuzz []
   (->>
     (range 1 101)
-    (map #(if (and (zero? (mod % 3)) (zero? (mod % 5))) "FizzBuzz" %))
-    (map #(if (and (not (string? %)) (zero? (mod % 3))) "Fizz" %))
-    (map #(if (and (not (string? %)) (zero? (mod % 5))) "Buzz" %))
+    (map #(if (and (multiple-of? % 3) (multiple-of? % 5)) "FizzBuzz" %))
+    (map #(if (and (not (string? %)) (multiple-of? % 3)) "Fizz" %))
+    (map #(if (and (not (string? %)) (multiple-of? % 5)) "Buzz" %))
     (map str)))
