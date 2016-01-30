b61f7ca28fe781c9c46741c45583a794db2d1b48
trikitrok <trikitrok@gmail.com>
2016-01-30 15:05:20 +0100

    Made first transformation more symmetrical

--- src/fizzbuzz_with_thread_macros/core.clj
+++ src/fizzbuzz_with_thread_macros/core.clj
@@ -6,7 +6,7 @@
 (defn fizzbuzz []
   (->>
     (range 1 101)
-    (map #(if (and (multiple-of? % 3) (multiple-of? % 5)) "FizzBuzz" %))
+    (map #(if (and (not (string? %)) (multiple-of? % 3) (multiple-of? % 5)) "FizzBuzz" %))
     (map #(if (and (not (string? %)) (multiple-of? % 3)) "Fizz" %))
     (map #(if (and (not (string? %)) (multiple-of? % 5)) "Buzz" %))
     (map str)))
